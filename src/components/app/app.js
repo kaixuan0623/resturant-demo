adee0aa50b3f52f21ac8027982068a82e9385238
KaixuanHu <kaixuan.hu@streamworx.ai>
2021-07-10 23:45:56 -0400

    init

---
+++ src/components/app/app.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react"
+import Order from "./order"
+import Steps from "./steps"
+
+import "./app.scss"
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      course: 0,
+      summary: false
+    }
+  }
+
+  handleCourse = (course = 0) => {
+    this.setState({ course })
+  }
+
+  handleSummary = () => {
+    this.setState({ summary: !this.state.summary })
+  }
+
+  render() {
+    return (
+      <main className="app">
+        <h1 className="title">
+          {this.state.summary ? "Your order: " : "Welcome to Bryan's Restaurant! Here is the Menu, enjoy!"}
+        </h1>
+        <Steps
+          {...this.state}
+          changeCourse={this.handleCourse}
+          summaryHandler={this.handleSummary}
+        />
+        <Order
+          {...this.state}
+          changeCourse={this.handleCourse}
+          summaryHandler={this.handleSummary}
+        />
+      </main>
+    )
+  }
+}
